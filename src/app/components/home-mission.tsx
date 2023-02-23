e62538af0e8f7f75c6bf24474624f991a19afa66
Matteo Gadola <gadola.matteo@gmail.com>
2023-02-23 17:30:31 +0100

    update

--- src/app/components/home-mission.tsx
+++ src/app/components/home-mission.tsx
@@ -6,11 +6,6 @@ import missionImage from 'public/images/mission-3new.jpg'
 import { base64 } from '@/lib/helpers'
 //import { base64 } from '@/lib/helpers'
 
-const description = `
-Il circuito GOinUP ripartirà il 24 maggio 2023 da Regoledo di Cosio
-con la Larg-Up giunta alla sua quinta edizione.
-Fino al 15 maggio sarà possibile acquistare il carnet dell'intero circuito (11 gare) al costo di 110€
-blablabla`
 
 // https://tailwindcomponents.com/component/tags
 //export default async function HomeBanner({ ticket }: { ticket: Ticket }) {
@@ -28,7 +23,7 @@ export default async function HomeMission() {
         <div className="text-slate-800 font-unbounded">
           <h1 className="text-2xl md:text-1xl text-accent">Finalità Benefica</h1>
           <h3 className="text-1xl md:text-4xl">Chi siamo?</h3>
-          <p>GOinUP ha finalità benefica, con una quota degli introiti dalle iscrizioni e dalle donazioni volontarie, si pone come obbiettivo quello di donare delle attrezzature e/o servizi all'associazione: VALTELLINA ACCESSIBILE DAPPERTUTTO</p>
+          <p>GOinUP ha finalità benefica, con una quota degli introiti dalle iscrizioni e dalle donazioni volontarie, si pone come obbiettivo quello di donare delle attrezzature e/o servizi all&apos;associazione: VALTELLINA ACCESSIBILE DAPPERTUTTO</p>
         </div>
 
         
