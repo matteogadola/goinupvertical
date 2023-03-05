047e274c551c8499cbc1e0e57151b20f4839adf5
Matteo Gadola <gadola.matteo@gmail.com>
2023-03-05 19:13:10 +0100

    Update

--- next.config.js
+++ next.config.js
@@ -4,6 +4,9 @@ const nextConfig = {
     appDir: true,
     fontLoaders: [{ loader: '@next/font/google', options: { subsets: ['latin'] } }],
   },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
