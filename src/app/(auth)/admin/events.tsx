b64b764fd0c359f2788a087ab555f1ecee14c3a8
Matteo Gadola <gadola.matteo@gmail.com>
2023-04-05 19:19:05 +0200

    Update

--- src/app/(auth)/admin/events.tsx
+++ src/app/(auth)/admin/events.tsx
@@ -1,28 +1,12 @@
 'use client';
 
-import { getItem } from '@/lib/items'
-import Link from 'next/link'
-import Image from 'next/image'
-
-import { base64 } from '@/lib/helpers'
 import { Suspense, useState } from 'react'
-import { getEvents } from '@/lib/events'
-import { dt, getDate } from '@/lib/date'
-//import { base64 } from '@/lib/helpers'
-
-import goinup from 'public/images/credits/goinup.png'
-
 import classNames from 'classnames'
 import Spinner from '@/components/spinner'
-import { getOrders, getEntries } from '@/app/lib/views'
 import { Event } from '@/types/events'
-import { useSupabase } from '@/app/components/supabase-provider';
 import EventContent from './event-content';
 
-// https://tailwindcomponents.com/component/tags
-//export default async function HomeBanner({ ticket }: { ticket: Ticket }) {
 export default function Events({ events, className }: { events: Event[], className?: string }) {
-  const { supabase, session } = useSupabase()
   const [event, setEvent] = useState<Event | undefined>(undefined)
 
   const selectEvent = (event: Event) => {
@@ -32,24 +16,23 @@ export default function Events({ events, className }: { events: Event[], classNa
   return (
     <Suspense fallback={<Spinner />}>
       { events?.length &&
-      <section className={classNames(className, "flex")}>
+        <section className={classNames(className, "flex")}>
 
-        <div>
-          <ul className="separator">
-          { events.map((item, index) =>
-            <li key={index} className="py-2 whitespace-nowrap">
-              <button onClick={() => selectEvent(item)} className={classNames({'font-semibold': event?.id === item.id}, "hover:opacity-80")}>{item.name}</button>
-            </li>
-          )}
-          </ul>
-        </div>
+          <div>
+            <ul className="separator">
+            { events.map((item, index) =>
+              <li key={index} className="py-2 whitespace-nowrap">
+                <button onClick={() => selectEvent(item)} className={classNames({'font-semibold': event?.id === item.id}, "hover:opacity-80")}>{item.name}</button>
+              </li>
+            )}
+            </ul>
+          </div>
 
-        <div className="pl-10 pt-1">
-          <EventContent event={event} />
-        </div>
+          <div className="pl-10 pt-1">
+            <EventContent event={event} />
+          </div>
 
-        
-      </section>
+        </section>
       }
     </Suspense>
   )
