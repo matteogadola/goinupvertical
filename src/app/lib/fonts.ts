48055073f08af32958eec0bdcad399683167c214
Matteo Gadola <gadola.matteo@gmail.com>
2023-05-08 09:23:13 +0200

    update deps

--- src/app/lib/fonts.ts
+++ src/app/lib/fonts.ts
@@ -3,22 +3,18 @@ import { Unbounded, Inter, Roboto_Mono, Poppins } from 'next/font/google';
 export const unbounded = Unbounded({
   display: 'swap',
   subsets: ['latin'],
-  variable: '--font-unbounded',
 });
 
 const poppins = Poppins({
-  variable: '--font-poppins',
   subsets: ['latin'],
   weight: ['400'],
 });
 
 const inter = Inter({
-  variable: '--font-inter',
   subsets: ['latin'],
 });
 
 const roboto_mono = Roboto_Mono({
-  variable: '--font-roboto-mono',
   subsets: ['latin'],
 });
 
