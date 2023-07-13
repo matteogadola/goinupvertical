fbc162976cf66aea1251e887e514c811431dfff4
Matteo Gadola <gadola.matteo@gmail.com>
2023-07-13 14:15:33 +0200

    update

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -16,8 +16,8 @@ export default function Footer({ promoter }: Props) {
 
         <div className="order-last md:order-none text-center">
           <p className="text-sm"><Link href="/contact" className="hover:opacity-60">Contatti</Link> | <Link href="/legal/terms" className="hover:opacity-60">Termini e Condizioni</Link> | <Link href="/legal/privacy-policy" className="hover:opacity-60">Informativa sulla Privacy</Link></p>
-          <p className="block text-xs text-slate-400">© 2023 GOinUP{/* | via Don Guanella 24, Morbegno P.IVA 00891820144*/}</p>
-          <p className="hidden text-xs text-slate-400">Realizzato da <a href="https://www.teeva.it" target="_blank" rel="noreferrer">Teeva</a></p>
+          {promoter === 'goinup' && <p className="block text-xs text-slate-400">© 2023 GOinUP{/* | via Don Guanella 24, Morbegno P.IVA 00891820144*/}</p>}
+          {/*<p className="hidden text-xs text-slate-400">Realizzato da <a href="https://www.teeva.it" target="_blank" rel="noreferrer">Teeva</a></p>*/}
         </div>
 
         <FooterLinks promoter={promoter} />
