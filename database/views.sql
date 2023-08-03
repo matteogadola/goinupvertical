a7448755dd2408d4db7e981fe8e638f174d41af7
Matteo Gadola <gadola.matteo@gmail.com>
2023-08-03 08:11:52 +0200

    update

--- database/views.sql
+++ database/views.sql
@@ -14,6 +14,7 @@ CREATE VIEW v_entries WITH (security_invoker) AS
     EXTRACT(year FROM e.birth_date) AS birth_year,
     e.gender,
     e.team,
+    e.fidal_card,
     e.email,
     e.phone_number
     FROM entries e
@@ -22,21 +23,6 @@ CREATE VIEW v_entries WITH (security_invoker) AS
       INNER JOIN items i ON oi.item_id = i.id
   WHERE o.payment_status = 'paid'::text OR (o.payment_method = 'cash'::text AND o.payment_status = 'pending'::text)
   ORDER BY e.last_name, e.first_name;
-
-  DROP VIEW IF EXISTS v_entries_public;
-
-  CREATE VIEW v_entries_public WITH (security_invoker) AS
-    SELECT e.event_id,
-      e.first_name,
-      e.last_name,
-      EXTRACT(year FROM e.birth_date) AS birth_year,
-      e.gender,
-      e.team
-    FROM entries e
-      INNER JOIN orders o ON e.order_id = o.id
-      INNER JOIN order_items oi ON e.order_item_id = oi.id
-    WHERE o.payment_status = 'paid'::text OR (o.payment_method = 'cash'::text AND o.payment_status = 'pending'::text)
-    ORDER BY e.last_name, e.first_name;
 
   --ALTER VIEW v_entries OWNER TO authenticated;
 
