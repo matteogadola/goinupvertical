66008e7f353802438125636c6af4bb1a4acb329f
Matteo Gadola <gadola.matteo@gmail.com>
2023-09-28 11:46:00 +0200

    fix: dynamic

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import '@/app/globals.css'
 import { fonts } from '@/lib/fonts'
 
-//export const dynamic = 'force-dynamic'
+export const dynamic = 'force-dynamic'
 
 export default async function RootLayout({
   children,
