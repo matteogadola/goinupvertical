1a6985bc59c6e88e59f2710e80094430d1960869
Matteo Gadola <gadola.matteo@gmail.com>
2024-04-18 10:27:04 +0200

    fix: date

--- src/app/(base)/(attachments)/media/page.tsx
+++ src/app/(base)/(attachments)/media/page.tsx
@@ -38,8 +38,12 @@ export default async function MediaPage() {
         {events.map(event =>
           <div key={event.id} className="w-full lg:w-1/3 p-6 shadow">
             <span className="overtitle">{event.edition}° {event.name}</span>
-            {event.category === 'race' &&
-              <span className="block text-xs text-gray-600">{dt(event.date).format('DD MMMM YYYY')}</span>
+            {event.category === 'race'
+              ? <>
+                <span className="overtitle">{event.edition}° {event.name}</span>
+                <span className="block text-xs text-gray-600">{dt(event.date).format('DD MMMM YYYY')}</span>
+              </>
+              : <span className="overtitle">{event.name} {dt(event.date).format('YYYY')}</span>
             }
 
             <div className="mt-4">
