23ffd4a9bcf6ba1e5c2ee11572f4d3a22c4a6991
matteo <magdanet@proton.me>
2025-04-08 09:29:25 +0200

    fix: confirm

--- src/app/(base)/checkout/confirm/page.tsx
+++ src/app/(base)/checkout/confirm/page.tsx
@@ -1,10 +1,9 @@
-import { Metadata, NextPage } from 'next'
-import Image from 'next/image'
-import { redirect, useSearchParams } from 'next/navigation'
-import { decodeBase64 } from '@/utils/encoding'
+import { GetServerSideProps, Metadata, NextPage } from 'next'
 //import { useCartStore } from '@store/cart';
-import { useEffect } from 'react';
 import DaEliminare from './da-eliminare';
+import Stripe from 'stripe';
+import { decodeBase64 } from '@/utils/encoding';
+import { notFound } from 'next/navigation';
 /*
 interface SearchParams {
   session_id?: string;
@@ -14,16 +13,60 @@ interface SearchParams {
 interface Props {
   searchParams: SearchParams;
 }*/
+// process.env.STRIPE_SECRET_KEY!
+const stripe = new Stripe('sk_test_51MHT1VJaZgjyJCpdcMKYInhMW6iYTLcgpnbuVMyWzrLkd6UCUdEESthbzPpMkwDaq9WLJbU8oSCBf8aYTFWEXQq900PaCLPJgw', {
+  apiVersion: '2025-02-24.acacia',
+});
 
-export default function CheckoutConfirmPage({ searchParams }: any) {
-  const order = decodeBase64<any>(searchParams.q);
-  console.log(order)
-  const detail = <></>
+export default async function CheckoutConfirmPage({ searchParams }: any) {
+  const { session_id, q } = await searchParams
 
-  //useEffect(() => useCartStore.setState({ items: [] }), [])
+  if (session_id && typeof session_id === 'string') {
+    try {
+      const session = await stripe.checkout.sessions.retrieve(session_id, {
+        expand: ['line_items.data.price.product'],
+      })
+      const items = session.line_items?.data ?? []
 
-  if (order?.items === undefined || !order.items.length) {
-    return (
+      return (
+        <>
+          <section>
+            <span className="overtitle">Conferma</span>
+            <h1 className="title mt-3">Ordine n. {session.metadata?.order_id}</h1>
+            <div className="text mt-2">
+              <table className="border border-slate-400 mt-8">
+                <thead>
+                  <tr className="bg-slate-200">
+                    <td className="w-80 border-b py-2">Riferimento</td>
+                    <td className="w-60 border-b py-2">Descrizione</td>
+                    <td className="w-32 border-b py-2">Quantità</td>
+                    <td className="w-32 border-b py-2">Prezzo</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  {
+                    items.map((item: any, index: any) =>
+                      <tr key={index}>
+                        <td className="py-2">{item.description}</td>
+                        <td className="py-2">{item.price.product.description}</td>
+                        <td className="py-2">{item.quantity}</td>
+                        <td className="py-2">{item.amount_total / 100}€</td>
+                      </tr>
+                    )
+                  }
+                </tbody>
+              </table>
+    
+              <p className="mt-8">A breve riceverai una mail con la conferma di pagamento.</p>
+            </div>
+          </section>
+          <DaEliminare />
+        </>
+      )
+    } catch (error) {
+      console.error(error);
+
+      return (
         <section>
           <span className="overtitle">Conferma</span>
           <div className="text mt-2">
@@ -31,60 +74,66 @@ export default function CheckoutConfirmPage({ searchParams }: any) {
             Riceverai a breve una mail di conferma.<br />
           </div>
         </section>
+      )
+    }
+    
+  } else if (q && typeof q === 'string') {
+    const order = decodeBase64<any>(q);
+    
+    return (
+      <>
+        <section>
+          <span className="overtitle">Conferma</span>
+          <h1 className="title mt-3">Ordine n. {order.id}</h1>
+          <div className="text mt-2">
+            <table className="border border-slate-400 mt-8">
+              <thead>
+                <tr className="bg-slate-200">
+                  <td className="w-80 border-b py-2">Riferimento</td>
+                  <td className="w-60 border-b py-2">Descrizione</td>
+                  <td className="w-32 border-b py-2">Quantità</td>
+                  <td className="w-32 border-b py-2">Prezzo</td>
+                </tr>
+              </thead>
+              <tbody>
+                {
+                  order.items.map((item: any, index: any) =>
+                    <tr key={index}>
+                      <td className="py-2">{item.name}</td>
+                      <td className="py-2">{item.description}</td>
+                      <td className="py-2">{item.quantity}</td>
+                      <td className="py-2">{item.price / 100}€</td>
+                    </tr>
+                  )
+                }
+              </tbody>
+            </table>
+            
+            {
+              order.items[0]?.name === 'Iscrizione 1^ Vertical Frasnedo' &&
+                <div className="mt-8">
+                  <span>Si prega di inviare copia del certificato medico, prima della gara, via mail a iscrizioni@teamvaltellina.com oppure consegnarlo presso il negozio 3Passi Patagonia</span>
+                </div>
+            }
+  
+            <p className="mt-8">A breve riceverai una mail con {
+              order.payment_method === 'sepa'
+                ? 'le istruzioni per poter effettuare il bonifico'
+                : order.payment_method === 'cash'
+                  ? 'le istruzioni per poter effettuare il pagamento in contanti'
+                  : order.payment_method === 'on-site'
+                    ? 'la conferma dell\'ordine'
+                    : 'la conferma di pagamento'
+            }
+              .</p>
+          </div>
+        </section>
+        <DaEliminare />
+      </>
     )
   }
 
-  return (
-    <>
-      <section>
-        <span className="overtitle">Conferma</span>
-        <h1 className="title mt-3">Ordine n. {order.id}</h1>
-        <div className="text mt-2">
-          <table className="border border-slate-400 mt-8">
-            <thead>
-              <tr className="bg-slate-200">
-                <td className="w-80 border-b py-2">Riferimento</td>
-                <td className="w-60 border-b py-2">Descrizione</td>
-                <td className="w-32 border-b py-2">Quantità</td>
-                <td className="w-32 border-b py-2">Prezzo</td>
-              </tr>
-            </thead>
-            <tbody>
-              {
-                order.items.map((item: any, index: any) =>
-                  <tr key={index}>
-                    <td className="py-2">{item.name}</td>
-                    <td className="py-2">{item.description}</td>
-                    <td className="py-2">{item.quantity}</td>
-                    <td className="py-2">{item.price / 100}€</td>
-                  </tr>
-                )
-              }
-            </tbody>
-          </table>
-          
-          {
-            order.items[0]?.name === 'Iscrizione 1^ Vertical Frasnedo' &&
-              <div className="mt-8">
-                <span>Si prega di inviare copia del certificato medico, prima della gara, via mail a iscrizioni@teamvaltellina.com oppure consegnarlo presso il negozio 3Passi Patagonia</span>
-              </div>
-          }
-
-          <p className="mt-8">A breve riceverai una mail con {
-            order.payment_method === 'sepa'
-              ? 'le istruzioni per poter effettuare il bonifico'
-              : order.payment_method === 'cash'
-                ? 'le istruzioni per poter effettuare il pagamento in contanti'
-                : order.payment_method === 'on-site'
-                  ? 'la conferma dell\'ordine'
-                  : 'la conferma di pagamento'
-          }
-            .</p>
-        </div>
-      </section>
-      <DaEliminare />
-    </>
-  )
+  notFound();
 }
 
 export const metadata: Metadata = {
