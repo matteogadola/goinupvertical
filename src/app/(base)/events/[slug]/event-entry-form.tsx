9aaa61fd30709633c6217645ef89000f46657ddb
matteo <magdanet@proton.me>
2025-04-09 18:42:01 +0200

    fix: entry_exist

--- src/app/(base)/events/[slug]/event-entry-form.tsx
+++ src/app/(base)/events/[slug]/event-entry-form.tsx
@@ -76,7 +76,7 @@ export default function EventEntryForm({ event, product }: { event: any, product
   const entryExist = async (event_id: string, tin: string) => {
     // da server...
     const { data, error } = await supabase.rpc('entry_exist', {
-      _event_id: '94134137-72eb-4824-9144-e3ccefda50cb',//event._id,
+      _event_id: event._id,
       _tin: tin,
       //_first_name: entry.first_name,
       //_last_name: entry.last_name,
@@ -114,7 +114,7 @@ export default function EventEntryForm({ event, product }: { event: any, product
     try {
       if (await entryExist(event.id, data.tin)) {
         // anche in questo caso meglio mostra errore generale?
-        form.setFieldError('tin', 'Codice fiscale già iscritto alla gara');
+        form.setFieldError('tin', 'Corridore già iscritto alla gara');
         return
       }
     } catch(e: any) {
