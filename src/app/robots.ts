644cabd7b68aa0ccf47bf8bee45788f493093e09
matteo <magdanet@proton.me>
2025-04-12 11:24:47 +0200

    fix: public

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,6 +4,12 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
+      allow: [
+        '/$',
+        '/favicon.ico',
+        '/photos',
+        '/results',
+      ],
       disallow: '/',
     },
     sitemap: 'https://goinupvertical.it/sitemap.xml',
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,25 +3,31 @@ import type { MetadataRoute } from 'next'
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: 'https://goinupvertical.it',
+      url: 'https://www.goinupvertical.it',
       lastModified: new Date(),
-      changeFrequency: 'yearly',
+      changeFrequency: 'monthly',
       priority: 1,
     },
-    {
+    /*{
       url: 'https://goinupvertical.it/about',
       lastModified: new Date(),
       changeFrequency: 'monthly',
       priority: 0.8,
     },
-    /*{
+    {
       url: 'https://goinupvertical.it/regulation',
       lastModified: new Date(),
       changeFrequency: 'yearly',
       priority: 0.5,
     },*/
     {
-      url: 'https://goinupvertical.it/media',
+      url: 'https://www.goinupvertical.it/photos',
+      lastModified: new Date(),
+      changeFrequency: 'weekly',
+      priority: 0.5,
+    },
+    {
+      url: 'https://www.goinupvertical.it/results',
       lastModified: new Date(),
       changeFrequency: 'weekly',
       priority: 0.5,
