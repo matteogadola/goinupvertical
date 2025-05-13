77697dc9a99cb5787c31a141d3a6b1a8dc625939
matteo <magdanet@proton.me>
2025-05-13 16:05:37 +0200

    fix: update version

--- src/utils/stripe/index.ts
+++ src/utils/stripe/index.ts
@@ -1,5 +1,5 @@
 import Stripe from 'stripe'
 
 export const createStripe = () => new Stripe(process.env.STRIPE_SECRET_KEY!, {
-  apiVersion: '2025-03-31.basil',
+  apiVersion: '2025-04-30.basil',
 })
