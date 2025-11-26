51f36e510f0b90c57635f44e4ce9d13ad9d955c4
matteo <magdanet@proton.me>
2025-11-26 12:52:41 +0100

    fix: update

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,6 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // a tendere va eliminato
-  },
   images: {
     remotePatterns: [new URL('https://cdn.sanity.io/images/5jqfesyl/production/**')],
   },
