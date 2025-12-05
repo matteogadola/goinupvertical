20fe5ecdc4d552ab917d7cd43ebd8013e6d9e63a
matteo <magdanet@proton.me>
2025-12-05 15:32:03 +0100

    fix: import

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,17 +1,12 @@
-import FadeUpAnimation from '@/components/animations/fade-up';
 import UpcomingEvents from './home-upcoming-events'
 import { getSerie, getFeatures, getUpcomingEvents } from '@/utils/sanity/queries'
 import HomeAboutUs from './home-about-us'
 import HomeMentions from './home-mentions'
 import HomeHero from './home-hero'
 import HomeFlyer from './home-flyer';
-import { FadeIn } from '@/components/animations/fade-in';
-import FadeUp from '@/components/animations/fade-up';
-import { ScrollAnimation, AnimationType } from '@/components/animations/scroll-animation';
-import { BookingHero } from './home-start-wrapper';
+import { ScrollAnimation } from '@/components/animations/scroll-animation';
 import HomeBanner from './home-banner'
 import HomeFeatures from './home-features';
-//import { BackgroundFeaturesSection, FeatureItem } from '@/components/feature-card/feature-card';
 
 export default async function Home() {
   const serie = await getSerie();
