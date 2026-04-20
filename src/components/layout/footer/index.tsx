b3cedf245ae84b375872afb2768dc3ea2b9e2ffd
matteo <magdanet@proton.me>
2026-04-20 19:06:44 +0200

    fix: prefetch

--- src/components/layout/footer/index.tsx
+++ src/components/layout/footer/index.tsx
@@ -9,7 +9,7 @@ export default function Footer() {
 
         <div className="order-last md:order-0 text-center">
           <p className="text-sm">
-            <Link href="/about" className="hover:text-blue-600">Contatti</Link> | <Link href="/legal/terms" className="hover:hover:text-blue-600">Termini e Condizioni</Link> | <Link href="/legal/privacy-policy" className="hover:hover:text-blue-600">Informativa sulla Privacy</Link>
+            <Link href="/about" prefetch={false} className="hover:text-blue-600">Contatti</Link> | <Link href="/legal/terms" prefetch={false} className="hover:hover:text-blue-600">Termini e Condizioni</Link> | <Link href="/legal/privacy-policy" prefetch={false} className="hover:hover:text-blue-600">Informativa sulla Privacy</Link>
           </p>
           <p className="block text-xs text-slate-400">GOinUP © {new Date().getFullYear()}</p>
         </div>
