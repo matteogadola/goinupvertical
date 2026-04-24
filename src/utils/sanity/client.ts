53291caba52d242dd05684ceaf65af98a8a10f5f
matteo <magdanet@proton.me>
2026-04-24 17:20:32 +0200

    fix: token

--- src/utils/sanity/client.ts
+++ src/utils/sanity/client.ts
@@ -5,5 +5,6 @@ export const client = createClient({
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
   apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-02-19',
   useCdn: process.env.DEPLOY_STAGE === 'production',
-  perspective: process.env.DEPLOY_STAGE === 'production' ? 'published' : 'drafts'
+  perspective: process.env.DEPLOY_STAGE === 'production' ? 'published' : 'drafts',
+  token: process.env?.SANITY_API_TOKEN,
 })
